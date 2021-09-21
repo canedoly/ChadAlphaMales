d0f26cdb6a5ed0df91ecd277ff329caa0afef0d0
lolnoenemy <buymypancakes@gmail.com>
2021-09-21 11:05:31 -0700

    Spin Speed for Spin AA

--- TeamFortress2/TeamFortress2/Features/Vars.h
+++ TeamFortress2/TeamFortress2/Features/Vars.h
@@ -457,6 +457,7 @@ namespace Vars
 		{
 			inline CVar<bool> Active	{ false, L"Active" };
 			inline CVar<int> Pitch		{ 0, L"Pitch" };		//0 - None, 1 - Up, 2 - Down, 3 - Fake Up, 4 - Fake Down
+			inline CVar<int> SpinSpeed	{ 0, L"Spin Speed" };
 			inline CVar<int> YawReal	{ 0, L"Yaw Real" };	//0 - None, 1 - Left, 2 - Right, 3 - Backwards
 			inline CVar<int> YawFake	{ 0, L"Yaw Fake" };	//0 - None, 1 - Left, 2 - Right, 3 - Backwards
 		}
