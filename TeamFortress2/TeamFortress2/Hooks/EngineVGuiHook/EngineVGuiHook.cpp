641d05a1cd7cdfa668925a2c232229293c3d84f5
Zephyr <kappamemez@gmail.com>
2021-09-22 19:19:53 +0300

    Oh... Forgot to change the version :(

--- TeamFortress2/TeamFortress2/Hooks/EngineVGuiHook/EngineVGuiHook.cpp
+++ TeamFortress2/TeamFortress2/Hooks/EngineVGuiHook/EngineVGuiHook.cpp
@@ -154,7 +154,7 @@ void __stdcall EngineVGuiHook::Paint::Hook(int mode)
 			}
 			int ms = std::max(0, (int)std::round(g_GlobalInfo.m_Latency * 1000.f));
 
-			_snwprintf(buff, sizeof(buff), _(L"CAM [v1.0] | fps: %i | delay: %ims"), fps, ms);
+			_snwprintf(buff, sizeof(buff), _(L"CAM [v1.1] | fps: %i | delay: %ims"), fps, ms);
 
 			g_Interfaces.Surface->GetTextSize(g_Draw.m_vecFonts[FONT_MENU].dwFont, buff, label_w, label_h);
 
